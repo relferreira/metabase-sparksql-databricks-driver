38968cdd989190f037209ea5eff9d8131031c473
Renan Lopes Ferreira <relferreira@gmail.com>
2020-04-07 10:07:52 -0300

    Update driver for time changes in 0.34

--- src/metabase/driver/sparksql_databricks.clj
+++ src/metabase/driver/sparksql_databricks.clj
@@ -22,22 +22,10 @@
             [metabase.query-processor
              [store :as qp.store]
              [util :as qputil]]
-            [metabase.util.honeysql-extensions :as hx]))
+            [metabase.util.honeysql-extensions :as hx])
+  (:import [java.sql Connection ResultSet]))
 
 (driver/register! :sparksql-databricks, :parent :hive-like)
-
-;;; +----------------------------------------------------------------------------------------------------------------+
-;;; |                                         metabase.driver.sql-jdbc impls                                         |
-;;; +----------------------------------------------------------------------------------------------------------------+
-
-;; (defmethod sql-jdbc.conn/connection-details->spec :sparksql-databricks [_ {:keys [host port db jdbc-flags], :as opts}]
-;;   (merge
-;;    {:classname                     "com.simba.spark.jdbc41.Driver" ; must be in classpath
-;;     :subprotocol                   "spark"
-;;     :subname                       (str "//" host ":" port "/" db jdbc-flags)
-;;     :ssl                           true}
-;;    (dissoc opts :host :port :db :jdbc-flags)))
-
 
 ;;; ------------------------------------------ Custom HoneySQL Clause Impls ------------------------------------------
 
@@ -81,12 +69,13 @@
     :or   {host "localhost", port 10000, db "", jdbc-flags ""}
     :as   opts}]
   (merge
-   {:classname                 "metabase.driver.FixedHiveDriver"
-    :subprotocol               "spark"
-    :subname                   (str "//" host ":" port "/" db jdbc-flags)}
+   {:classname                  "metabase.driver.FixedHiveDriver"
+    :subprotocol                "spark"
+    :subname                    (str "//" host ":" port "/" db jdbc-flags)}
    (dissoc opts :host :port :jdbc-flags)))
 
-(defmethod sql-jdbc.conn/connection-details->spec :sparksql-databricks [_ details]
+(defmethod sql-jdbc.conn/connection-details->spec :sparksql-databricks
+  [_ details]
   (-> details
       (update :port (fn [port]
                       (if (string? port)
@@ -136,31 +125,60 @@
            :database-type data-type
            :base-type     (sql-jdbc.sync/database-type->base-type :hive-like (keyword data-type))}))))})
 
-
-;; we need this because transactions are not supported in Hive 1.2.1
 ;; bound variables are not supported in Spark SQL (maybe not Hive either, haven't checked)
-(defmethod driver/execute-query :sparksql-databricks
-  [driver {:keys [database settings], query :native, :as outer-query}]
-  (let [query (-> (assoc query
-                    :remark (qputil/query->remark outer-query)
-                    :query  (if (seq (:params query))
-                              (unprepare/unprepare driver (cons (:query query) (:params query)))
-                              (:query query))
-                    :max-rows (mbql.u/query->max-rows-limit outer-query))
-                  (dissoc :params))]
-    (sql-jdbc.execute/do-with-try-catch
-      (fn []
-        (let [db-connection (sql-jdbc.conn/db->pooled-connection-spec database)]
-          (hive-like/run-query-without-timezone driver settings db-connection query))))))
+(defmethod driver/execute-reducible-query :sparksql-databricks
+  [driver {:keys [database settings], {sql :query, :keys [params], :as inner-query} :native, :as outer-query} context respond]
+  (let [inner-query (-> (assoc inner-query
+                               :remark (qputil/query->remark outer-query)
+                               :query  (if (seq params)
+                                         (unprepare/unprepare driver (cons sql params))
+                                         sql)
+                               :max-rows (mbql.u/query->max-rows-limit outer-query))
+                        (dissoc :params))
+        query       (assoc outer-query :native inner-query)]
+    ((get-method driver/execute-reducible-query :sql-jdbc) driver query context respond)))
 
-(defmethod driver/supports? [:sparksql-databricks :basic-aggregations]              [_ _] true)
-(defmethod driver/supports? [:sparksql-databricks :binning]                         [_ _] true)
-(defmethod driver/supports? [:sparksql-databricks :expression-aggregations]         [_ _] true)
-(defmethod driver/supports? [:sparksql-databricks :expressions]                     [_ _] true)
-(defmethod driver/supports? [:sparksql-databricks :native-parameters]               [_ _] true)
-(defmethod driver/supports? [:sparksql-databricks :nested-queries]                  [_ _] true)
-(defmethod driver/supports? [:sparksql-databricks :standard-deviation-aggregations] [_ _] true)
+;; 1.  SparkSQL doesn't support `.supportsTransactionIsolationLevel`
+;; 2.  SparkSQL doesn't support session timezones (at least our driver doesn't support it)
+;; 3.  SparkSQL doesn't support making connections read-only
+;; 4.  SparkSQL doesn't support setting the default result set holdability
+(defmethod sql-jdbc.execute/connection-with-timezone :sparksql-databricks
+  [driver database ^String timezone-id]
+  (let [conn (.getConnection (sql-jdbc.execute/datasource database))]
+    (try
+      (.setTransactionIsolation conn Connection/TRANSACTION_READ_UNCOMMITTED)
+      conn
+      (catch Throwable e
+        (.close conn)
+        (throw e)))))
 
-(defmethod driver/supports? [:sparksql-databricks :foreign-keys] [_ _] true)
+;; 1.  SparkSQL doesn't support setting holdability type to `CLOSE_CURSORS_AT_COMMIT`
+(defmethod sql-jdbc.execute/prepared-statement :sparksql-databricks
+  [driver ^Connection conn ^String sql params]
+  (let [stmt (.prepareStatement conn sql
+                                ResultSet/TYPE_FORWARD_ONLY
+                                ResultSet/CONCUR_READ_ONLY)]
+    (try
+      (.setFetchDirection stmt ResultSet/FETCH_FORWARD)
+      (sql-jdbc.execute/set-parameters! driver stmt params)
+      stmt
+      (catch Throwable e
+        (.close stmt)
+        (throw e)))))
+
+
+(doseq [feature [:basic-aggregations
+                 :binning
+                 :expression-aggregations
+                 :expressions
+                 :native-parameters
+                 :nested-queries
+                 :standard-deviation-aggregations]]
+  (defmethod driver/supports? [:sparksql-databricks feature] [_ _] true))
+
+;; only define an implementation for `:foreign-keys` if none exists already. In test extensions we define an alternate
+;; implementation, and we don't want to stomp over that if it was loaded already
+(when-not (get (methods driver/supports?) [:sparksql-databricks :foreign-keys])
+  (defmethod driver/supports? [:sparksql-databricks :foreign-keys] [_ _] true))
 
 (defmethod sql.qp/quote-style :sparksql-databricks [_] :mysql)
