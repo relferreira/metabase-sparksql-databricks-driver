76206598941467e1d4813e30e7a2944b447336f1
Léon Stefani <leon@brigad.co>
2024-02-05 10:20:36 +0100

    fix: pr comms (#5)
    * fix: pr comms
    
    * fix: jar naming
    
    ---------
    
    Co-authored-by: Stefani <>

--- scripts/extract_plugin.sh
+++ scripts/extract_plugin.sh
@@ -2,6 +2,5 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 docker buildx build --build-arg METABASE_VERSION=v0.48.3 --target stg_export --platform "linux/arm64" -t metabase:databricks-plugin "$SCRIPT_DIR/.."
 container_id=$(docker create "metabase:databricks-plugin" /bin/bash)
-docker cp "$container_id:/sparksql-databricks.metabase-driver.jar" "$SCRIPT_DIR/../dist/sparksql-databricks.metabase-driver.jar"
+docker cp "$container_id:/sparksql-databricks-v2.metabase-driver.jar" "$SCRIPT_DIR/../dist/databricks-sql.metabase-driver.jar"
 docker rm "$container_id"
-
